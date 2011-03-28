bca5ee2291d174aa4444f75c537f7d4389b7a093
Michael Klett <michael@webadvocate.com>
2011-03-28 12:04:03 -0400

    Example app

---
+++ chargify_direct_signature_example.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'openssl'
+require 'addressable/uri'
+
+HMAC_SHA1_DIGEST = OpenSSL::Digest::Digest.new('sha1')
+
+get '/' do
+  erb :index
+end
+
+post '/compute' do
+  signature = hexdigest(params[:api_id], params[:timestamp], params[:nonce], params[:data], params[:api_secret])
+  uri = Addressable::URI.new
+  uri.query_values = {
+    :api_id => params[:api_id],
+    :timestamp => params[:timestamp],
+    :nonce => params[:nonce],
+    :data => params[:data],
+    :api_secret => params[:api_secret],
+    :signature => signature
+  }
+  uri.path = "/"
+  
+  redirect uri.to_s
+end
+
+post '/verify' do
+  @expected_signature = hexdigest(params[:secure][:api_id], params[:secure][:timestamp], params[:secure][:nonce], params[:secure][:data], params[:api_secret])
+  @actual_signature = params[:secure][:signature]
+  erb :verify
+end
+
+def hexdigest(api_id, timestamp, nonce, data, secret)
+  message = "#{api_id}#{timestamp}#{nonce}#{data}"
+  OpenSSL::HMAC.hexdigest(HMAC_SHA1_DIGEST, secret.to_s, message)
+end
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'chargify_direct_signature_example'
+run Sinatra::Application
